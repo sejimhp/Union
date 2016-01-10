8ee14a479f2c15f302922dc697cb455c18487245
sejimhp <sejimhp@gmail.com>
2016-01-10 14:49:04 +0900

    add actorManager

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Union
-シューティングゲーム
+敵を仲間にしていく感じのシューティングゲーム  
+
+[@yashihei][https://github.com/yashihei]のコードを参考にしながら作成します
--- Union/Actor.h
+++ Union/Actor.h
@@ -16,3 +16,33 @@ public:
 private:
 	bool enabled = true;
 };
+
+template<typename Type>
+class ActorManager{
+public:
+	ActorManager() = default;
+	virtual ~ActorManager() = default;
+
+	void add(std::shared_ptr<Type> actor){
+		actora.emplace_back(actor);
+	}
+	void clear(){
+		actors.clear();
+	}
+	int size(){
+		return actors.size();
+	}
+	virtual void update(Game* game){
+		for (auto& actor : actors){
+			actor->update(game);
+		}
+		Erase_if(actors, [](std::shared_ptr<Type> actor){return !actor->isEnabled; });
+	}
+	virtual void draw(Game* game){
+		for (auto& actor : actors){
+			actor->draw(game);
+		}
+	}
+private:
+	std::list<std::shared_ptr<Type>> actors;
+};
